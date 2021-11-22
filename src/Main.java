cf1aa3944aa087146584e28b719f5f96657c57d4
Jv7Pinheiro <deolj19@wfu.edu>
2021-11-22 11:43:12 -0500

    Changed Titled

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,13 @@ public class Main {
         JFrame obj = new JFrame();
         Gameplay gameplay = new Gameplay();
         obj.setBounds(10, 10, 700, 600);
-        obj.setTitle("WFU Brick Breaker");
+        obj.setTitle("LEZGOOOOOOOOO IT WORKEDDDD");
         obj.setResizable(false);
 
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.add(gameplay);
         obj.setVisible(true);   // This line should be listed after adding the gameplay object
 
-
+        System.out.println("Never gonna give you up, never gonna let you down, never gonna turn around and desert you");
     }
 }
