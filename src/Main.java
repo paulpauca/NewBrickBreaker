6ba61b81f62808b92c3c6efe82d34d194bbd5182
Paul Pauca <paucavp@wfu.edu>
2021-11-22 11:28:30 -0500

    Initial commit of Brick Breaker game

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay gameplay = new Gameplay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+        obj.setVisible(true);
+
+
+    }
+}
