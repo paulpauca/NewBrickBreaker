d970f51435395cc57ae325a048ee3b658b9f04c7
Paul Pauca <paucavp@wfu.edu>
2021-11-22 11:37:55 -0500

    Added some important comments to Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+/**
+ * This class implements the Brick Breaker game following the onlihe tutorial
+ *
+ * @author Paul Pauca
+ */
+
 import javax.swing.*;
 
 public class Main {
@@ -10,7 +16,7 @@ public class Main {
 
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.add(gameplay);
-        obj.setVisible(true);
+        obj.setVisible(true);   // This line should be listed after adding the gameplay object
 
 
     }
